05dc21c02a64fe2b7d120e666e65e1e795d9abfa
Mioty Rajosefa <hei.mioty.3@gmail.com>
2026-08-20 10:43:30 +0300

    first commit

---
+++ src/test/java/com/example/vibeCoding/VibeCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vibeCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
